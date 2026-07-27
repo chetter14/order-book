12890e5a6ab525e2b44b9b5d60de185ea0bbb444
Artem Leshchukov <artemleshchukov02@gmail.com>
2026-07-27 18:23:08 +0300

    Continue implementing logger, draft version

--- modules/logger/include/logger.h
+++ modules/logger/include/logger.h
@@ -1,9 +1,18 @@
 #ifndef ORDER_BOOK_LOGGER_H
 #define ORDER_BOOK_LOGGER_H
 
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <print>
 #include "order_book.h"
 
 namespace ob_logger {
+
+/* Not used now; for future extensions */
+enum class LogLevel { INFO, WARNING, ERROR };
 
 struct ExecutedOrder {
   ob::UserId buyer;
@@ -15,9 +24,35 @@ struct ExecutedOrder {
 
 class Logger {
  public:
-  Logger();
+  explicit Logger(const std::filesystem::path& path)
+      : out_{path, std::ios::app} {
+    if (!out_) {
+      std::cerr << "Failed to open the file - " << path.string() << " !"
+                << std::endl;
+      exit(1);
+    }
+  };
 
-  void recordExecutedOrder(const ExecutedOrder& order);
+  Logger(const Logger&) = delete;
+  Logger& operator=(const Logger&) = delete;
+
+  void recordExecutedOrder(const ExecutedOrder& order) {
+    const auto now = std::chrono::floor<std::chrono::milliseconds>(
+        std::chrono::system_clock::now());
+
+    const std::chrono::zoned_time timestamp{std::chrono::current_zone(), now};
+
+    std::lock_guard lock{mtx_};
+    std::println(out_,
+                 "[{}] OPERATION={}, BUYER={}, SELLER={}, PRICE={}, AMOUNT={}",
+                 timestamp, ob::to_string(order.type), order.buyer,
+                 order.seller, order.price, order.amount);
+    out_.flush();
+  }
+
+ private:
+  std::ofstream out_;
+  std::mutex mtx_;
 };
 
 }  // namespace ob_logger
--- modules/logger/tests/logger_tests.cpp
+++ modules/logger/tests/logger_tests.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 #include "logger.h"
 
-TEST(Logger, Mock) {
+/* TO-DO: proper tests are required */
+
+TEST(Logger, TestLogging) {
+  ob_logger::Logger logger{"test_file.txt"};
+
+  logger.recordExecutedOrder(
+      ob_logger::ExecutedOrder{1, 2, ob::OrderType::BUY, 120, 5});
+
   ASSERT_EQ(1, 1);
 }
